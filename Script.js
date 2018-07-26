664e5ff3a641d96711fe1982397dd00faed7996e
Lukasz Borek <borek_lukasz@outlook.com>
2018-07-26 17:29:02 +0200

    Update files

--- Script.js
+++ Script.js
@@ -22,13 +22,36 @@ var Counter = React.createClass({
         })
     },
 
+    componentWillMount: function() {
+        console.log('Wykonałem się zaraz przed metodą render.')
+    },
+    componentDidMount: function() {
+      console.log('Pokazuje komponentu na stronie.')  
+    },
+    componentWillReceiveProps: function() {
+        console.log('Wykonam się gdy dostenę nowe właściwości')
+    },
+    shouldComponentUpdate: function() {
+        console.log('Sprawdzam czy faktycznie muszę przersyować komponent.')
+        return true;
+    },
+    componentWillUpdate: function() {
+        console.log('Przygotowuje nadchodzace zmiany.')
+    },
+    componentDidUpdate: function() {
+      console.log('Wyświetlam zaktualizowany komponent')  
+    },
+    componentWillUnmount: function() {
+        console.log('Usuwam zbędny komponent')
+    },
+    
     render: function() {
       return (
-        React.createElement('div', {className: 'counter'}, 
-          React.createElement('div', {className: 'btn-group'}, 
-            React.createElement('button', {className: 'btn',onClick: this.decrement}, '-'),
+        React.createElement('div', {className: 'container'}, 
+          React.createElement('div', {className: 'counter'}, 
+            React.createElement('button', {className: 'btn-up',onClick: this.increment}, '+'),
             React.createElement('div', {}, 'Counter: ' + this.state.counter),
-            React.createElement('button', {className: 'btn', onClick: this.increment}, '+'),
+            React.createElement('button', {className: 'btn-down', onClick: this.decrement}, '-'),
           )
         )
       );
@@ -36,6 +59,7 @@ var Counter = React.createClass({
 });
 
 var element = React.createElement('div', {}, 
+    React.createElement(Counter, {}),
     React.createElement(Counter, {})
 );
 
